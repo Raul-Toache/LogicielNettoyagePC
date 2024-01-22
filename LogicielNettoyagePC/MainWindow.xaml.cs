9c2f7cc7b2b820d4f52fde55b6f9bb447079d7cd
Raul-Toache <rauleduardotoachethomas@gmail.com>
2024-01-22 02:09:00 -0600

    Completed the GUI and added some basic functionalities.

--- LogicielNettoyagePC/MainWindow.xaml.cs
+++ LogicielNettoyagePC/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,41 @@ namespace LogicielNettoyagePC
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void analyse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clean_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void history_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO: Create history page", "History", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void udpate_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You are running the latest version", "Update", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void website_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/Raul-Toache")
+                {
+                    UseShellExecute = true
+                });
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: ", ex.Message);
+            }
         }
     }
 }
